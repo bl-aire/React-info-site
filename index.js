15d6dfa4c17781916de3009d3cdd906a35648971
Blaire <ogoh_blessing@yahoo.com>
2022-08-20 22:56:09 +0100

    Create page markup

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const navigation =
-   (
-        <nav>
-           <h1>Blaire</h1>
-           <ul>
-                <li>Services</li>
-                <li>About</li>
-                <li>Contact</li>
-           </ul>
-        </nav>
-    )
 
+const page = (
+    <div className="m-4">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="40px" className="mb-3" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
 
-ReactDOM.render(navigation, document.getElementById("root"))
+ReactDOM.render(page , document.getElementById("root"))
