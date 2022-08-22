c7e2f03d7f712a3fbc27341d5d498e3157e2c5b4
Blaire <ogoh_blessing@yahoo.com>
2022-08-22 15:30:46 +0100

    Organize components

---
+++ Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return ( 
+        <footer className="footer">
+            <small>@2021 Blaire development. All rights reserved.</small>
+        </footer>
+    )
+}
--- index.js
+++ index.js
@@ -1,59 +1,17 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-/*const page = (
-    <div className="m-4">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="40px" className="mb-3" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)*/
-
-function Header() {
+function App(){
     return(
-        <header>
-            <nav className="nav">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"  className="mb-3 nav-logo" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <div className="main">
-            <h1>Fun facts about React</h1>
-            <ol>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ol>
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }
 
-function Footer() {
-    return ( 
-        <footer className="footer">
-            <small>@2021 Blaire development. All rights reserved.</small>
-        </footer>
-    )
-}
-
 ReactDOM.render(
-    <div>
-        <Header />
-        <MainContent />
-        <Footer />
-    </div>, document.getElementById("root"))
+    <App />, document.getElementById("root")
+)
