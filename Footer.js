da9d7397b3a1c86364df3028563a64e3280b73b5
Blaire <ogoh_blessing@yahoo.com>
2022-08-22 17:30:55 +0100

    Organize directory

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return ( 
-        <footer className="footer">
-            <small>@2021 Blaire development. All rights reserved.</small>
-        </footer>
-    )
-}
--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="nav">
+          <img src={logo}  className="mb-3 nav-logo App-logo" alt="logo" />
+            <ul className="nav-items">
+              <li>Pricing</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+        </nav>
       </header>
+
+      <div className="main">
+        <h1>Fun facts about React</h1>
+          <ol>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ol>
+      </div>
+
+      <footer className="footer">
+            <small>@2021 Blaire development. All rights reserved.</small>
+        </footer>
     </div>
   );
 }
